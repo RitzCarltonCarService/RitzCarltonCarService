020c434f8c915087901a255a661ef4b81d7de476
TAYLOR GEORGE <ATGEORGE@UTEXAS.EDU>
2020-01-09 16:48:04 -0600

    add white box

--- src/layouts/Home-SubComponents/MainScreen.js
+++ src/layouts/Home-SubComponents/MainScreen.js
@@ -2,27 +2,30 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { navigate } from '../../redux/actions';
 import { View, Text, Button } from 'react-native';
+import TheWhiteSquare from '../../components/TheWhiteSquare';
 
 const MainScreen = props => {
 
     return (
         <View>
-            <Text>
-                Scheduled Pickups:
-            </Text>
-            {props.scheduledPickups.length > 0 ? 
-            props.scheduledPickups.map(pickup => {
-                return (
-                    <Button
-                        title={pickup.name}
-                        onPress={() => { props.setPage("pickup info") }}
-                    />
-                )
-            }) :
-            <Text>
-                No Current Requests
-            </Text>
-            }
+            <TheWhiteSquare>
+                <Text>
+                    Scheduled Pickups:
+                </Text>
+                {props.scheduledPickups.length > 0 ? 
+                props.scheduledPickups.map(pickup => {
+                    return (
+                        <Button
+                            title={pickup.name}
+                            onPress={() => { props.setPage("pickup info") }}
+                        />
+                    )
+                }) :
+                <Text>
+                    No Current Requests
+                </Text>
+                }
+            </TheWhiteSquare>
             <Button
                 title="New Pickup"
                 onPress={() => { props.setPage("new pickup") }}
