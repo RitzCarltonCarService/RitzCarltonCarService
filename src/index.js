3079f0e9be2b9e38996feb74f033954b58af974e
Keenan Johns <39966609+FluxxField@users.noreply.github.com>
2020-01-09 08:58:17 -0600

    Working base (#7)
    * Branch setup
    
    * File structure setup
    
    * Working repo
    
    * Working base

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import AuthLoadingScreen from './screens/AuthLoadingScreen';
+import Dashboard from './screens/Dashboard';
 import Login from './screens/Login';
 
 const Router = createStackNavigator(
    {
-      Login
+      AuthLoadingScreen,
+      Dashboard,
+      Login,
    },
    {
       initialRouteName: "AuthLoadingScreen",
---
+++ src/screens/Dashboard.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { navigate, toHome } from '../redux/actions';
+import { View, Text, Button } from 'react-native';
+import MainScreen from '../layouts/Home-SubComponents/MainScreen';
+import NewPickup from '../layouts/Home-SubComponents/NewPickup';
+import PrePickupInfo from '../layouts/PickupInfo-SubComponents/PrePickupInfo';
+
+const Home = props => {
+   const [page, setPage] = useState("home");
+
+   switch (page) {
+      case "new pickup":
+         return (
+            <View>
+               <NewPickup setPage={setPage} />
+            </View>
+         )
+      case "pickup info":
+         return (
+            <View>
+               <PrePickupInfo
+                  setPage={setPage}
+               />
+            </View>
+         )
+      default:
+         return (
+            <View>
+               <MainScreen setPage={setPage} />
+            </View>
+         )
+   }
+}
+
+const mapStateToProps = state => {
+   return {
+      nav: state.nav
+   }
+}
+
+const mapDispatchToProps = {
+   navigate: navigate
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
