532add778188cf893827a6673ef9251f667c1359
jkeane889 <jkeane889@gmail.com>
2020-01-10 14:52:50 -0600

    Updated Location form screen

--- src/layouts/Home-SubComponents/NewPickup-SubComponents/LocationForm.js
+++ src/layouts/Home-SubComponents/NewPickup-SubComponents/LocationForm.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { navigate } from '../../../redux/actions';
-import { View, Text, Button } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import TextInput from '../../../components/TextInput.js'
+import Button from '../../../components/Button.js'
 
 const LocationForm = props => {
     return (
         <View>
-            <Text>
-                Form 0
-            </Text>
-            <Button
-                title="Next"
-                onPress={() => { props.setForm(1) }}
-            />
+            <TextInput style={styles.inputTo} label='To:'/>
+            <TextInput style={styles.inputFrom} label='From:'/>
+            <TextInput style={styles.inputTime} label='Date:'/>
+            <Button style={styles.confirmButton}>Confirm</Button>
+            <Button style={styles.backButton}>Back</Button>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    inputTo: {
+        width: '300%',
+        alignSelf: 'center'
+    },
+    inputFrom: {
+        width: '300%',
+        alignSelf: 'center'
+    },
+    inputTime: {
+        width: '300%',
+        alignSelf: 'center',
+        marginBottom: '275%'
+    },
+    confirmButton: {
+        width: '300%',
+        alignSelf: 'center'
+    },
+    backButton: {
+        width: '300%',
+        alignSelf: 'center'
+    }
+});
 
 const mapDispatchToProps = {
     navigate: navigate
