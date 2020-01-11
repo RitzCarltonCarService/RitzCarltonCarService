b244b1ab2cbe9513600c27d21c4992f1e6158366
MarioMorales7x7 <mario.morales.texas@gmail.com>
2020-01-11 17:55:08 -0600

    status updates added with case/if

--- src/screens/DriverDash.js
+++ src/screens/DriverDash.js
@@ -1,5 +1,5 @@
 import React, { memo, useState, useEffect } from "react";
-import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, View, Picker } from "react-native";
 import MapBackground from "../components/MapBackground";
 import Logo from "../components/Logo";
 import Header from "../components/Header";
@@ -10,10 +10,23 @@ import TheWhiteSquare from '../components/TheWhiteSquare';
 import { theme } from "../core/theme";
 import { emailValidator, passwordValidator } from "../core/untilities";
 import Toast from "../components/Toast";
+import ModalDropdown from 'react-native-modal-dropdown';
 import firebase from 'firebase';
 
 const DriverDash = ({ navigation }) => {
    const [date, setDate] = useState(new Date());
+   const [punch, setPunch] = useState({ val: "Clocked Out" });
+   const [dropdownVal, setdropdownVal] = useState({ ddv: "xxx" });
+
+   const dropdownOptions = [
+     'CLOCK IN',
+     'CLOCK OUT',
+     'START BREAK',
+     'END BREAK',
+     'START MEAL',
+     'END MEAL',
+    ]
+
    useEffect(() => {
     var timer = setInterval( () => tick(), 1000 );
  
@@ -25,6 +38,34 @@ const DriverDash = ({ navigation }) => {
    function tick() {
     setDate(new Date());
    }
+
+   function statusUpdate() {
+    switch (dropdownVal.ddv) {
+      case '1':
+        punch.val = "Clocked Out";
+        break;
+      case '2':
+        punch.val = "On Break";
+        break;
+      case '4':
+        punch.val = "On Meal Time";
+        break;
+      default:
+        punch.val = "Clocked In";
+    }
+    // const d = dropdownVal.ddv;
+    // if (d == 1) {
+    //   punch.val = "Clocked Out";
+    // } else if (d == 2) {
+    //   punch.val = "On Break";
+    // } else if (d == 4) {
+    //   punch.val = "On Meal Time";
+    // } else {
+    //   punch.val = "Clocked In";
+    // }
+    //punch.val = dropdownOptions[dropdownVal.ddv];
+   }
+
    return (
       <>
          <MapBackground />
@@ -35,11 +76,19 @@ const DriverDash = ({ navigation }) => {
             <TheWhiteSquare height={75} top={15}>
                <Logo />
 
-               <Header>Status: Clocked Out</Header>
+               <Header>Status: { punch.val }</Header>
                <Header>{date.toLocaleTimeString()}</Header>
+               <ModalDropdown 
+                defaultValue={ punch.val === 'Clocked In' ? 'CLOCK OUT' : 'CLOCK IN' }
+                options={dropdownOptions}
+                style={styles.picker}
+                textStyle={styles.picker_text}
+                dropdownStyle={styles.picker_dropdown}
+                onSelect={(value) => dropdownVal.ddv = value}
+               />
 
                {/* onPress={_onLoginPressed} */}
-               <Button mode="contained" >
+               <Button mode="contained" onPress={statusUpdate}>
                   Submit
                </Button>
             </TheWhiteSquare>
@@ -69,7 +118,34 @@ const styles = StyleSheet.create({
    },
    wrapper: {
       alignItems: 'center'
-   }
+   },
+   picker: {
+    width: 200,
+    borderWidth: 0,
+    borderRadius: 3,
+    backgroundColor: 'black',
+  },
+  picker_text: {
+    fontFamily: Platform.OS === 'ios' ? "Arial" : "Roboto",
+    letterSpacing: 2,
+    fontWeight: "bold",
+    fontSize: 17,
+    lineHeight: 26,
+    color: theme.colors.secondary,
+    marginVertical: 10,
+    marginHorizontal: 6,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  picker_dropdown: {
+    width: 200,
+    height: 200,
+    fontWeight: "bold",
+    borderColor: 'darkgrey',
+    borderWidth: 2,
+    borderRadius: 3,
+    backgroundColor: 'lightgrey',
+  },
 });
 
 export default memo(DriverDash);
