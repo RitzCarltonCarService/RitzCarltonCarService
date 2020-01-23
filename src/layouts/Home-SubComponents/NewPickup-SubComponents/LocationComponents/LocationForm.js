f75fce09cbbe514b332f024971e9f805a6c64a00
Jonathan Keane <jkeane889@gmail.com>
2020-01-23 10:16:27 -0600

    Removed API keys (#43)
    * Updated Location form screen
    
    * Added detailed comments to all of the components (#17)
    
    * Branch setup
    
    * File structure setup
    
    * Working repo
    
    * Working base
    
    * Enhancments
    
    * Working background component
    
    * Created MapBackground component
    
    * Updated HomeScreen
    
    * fixed a node_modules package
    
    * Working LoginScreen with skeleton Signup Screen
    
    * Component comments
    
    * Fixed missing Background.js error (#20)
    
    * Branch setup
    
    * File structure setup
    
    * Working repo
    
    * Working base
    
    * Enhancments
    
    * Working background component
    
    * Created MapBackground component
    
    * Updated HomeScreen
    
    * fixed a node_modules package
    
    * Working LoginScreen with skeleton Signup Screen
    
    * Component comments
    
    * Fixed missing Background.js
    
    * Fixed missing Background.js file issue
    
    * Arial fix
    
    * Taylordev (#21)
    
    * update scheduled pickups on main screen
    
    * add white box
    
    * add map
    
    * clean up map
    
    * redoing yesterday
    
    * back up to date
    
    * added ... to pickupEntries
    
    * FOR ROB
    
    * Updated Travis CI with Jest testing integration
    
    * Current background map version with geolocation enabled
    
    * Updated geolocation component for app
    
    * Geolocation completed
    
    * Updated geolocation
    
    * removed merge conflicts
    
    * Updated locationForm component
    
    * Untrack files in .gitignore
    
    * cleared cache for API key
    
    * cleared cache for API key
    
    * Imported Date picker and hooks for to/from/date components
    
    * Imported Date picker and hooks for to/from/date components
    
    * Finished autocomplete and drop down, updating Hooks state with selection and clearing dropdown
    
    * Component before changing formatting
    
    * Updated LocationForm styling and functionality
    
    * Updated Location Form Component
    
    * Location form before commit
    
    * Final logic for location To/From text input, autocomplete component
    
    * Edited Location Form component
    
    * Updated location form and redux to manage selected to and from destinations
    
    * Removed merge conflicts and updated file with API keys
    
    * Updated Map Background component to build foundation for route optimization
    
    * Updated map component with styling and functionality
    
    * Updated date&time picker
    
    * Updated DateTimePicker and rendering functionality
    
    * Updated MapComponent with Redux functionality
    
    * Updated locationForm to use default location via axios request
    
    * Updated functionalities of To and From text input boxes for Location Form
    
    * Updated styling for Location Form
    
    * Split Location Form components up into subcomponents for improved conditional rendering
    
    * Split Location Form components up into subcomponents for improved conditional rendering
    
    * Refactoring of Location components
    
    * Updated styling for refactored LocationForm components
    
    * Updated styling for Location Form components
    
    * Updated Location form screen for spacing
    
    * Updated Date/Time selector
    
    * Updated styling for locationForm
    
    * Updated Location form components
    
    * Styling for LocationForm
    
    * Before testing location form and map
    
    * Fixed missing redux props for Dashboard screen
    
    * Updated location form with state setting functionality for time and date confirmation
    
    * Updated location and time selection components
    
    * Delete API keys
    
    Co-authored-by: Keenan Johns <39966609+FluxxField@users.noreply.github.com>
    Co-authored-by: atgeorge11 <53580812+atgeorge11@users.noreply.github.com>

--- src/layouts/Home-SubComponents/NewPickup-SubComponents/LocationComponents/LocationForm.js
+++ src/layouts/Home-SubComponents/NewPickup-SubComponents/LocationComponents/LocationForm.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import Moment from 'react-moment';
+import Moment from 'moment';
 import { connect } from 'react-redux';
 import { updateFromLocation } from '../../../../redux/actions';
 import React, { useState, memo } from 'react';
@@ -14,21 +14,8 @@ const LocationForm = ({ updateFromLocation, ...props }) => {
     // MAKE SURE TO REMOVE GOOGLE MAPS API KEY BEFORE PUSHING TO GIT HUB!!!!!!!!
 
     // REMEMBER TO ADD API KEY IF YOU WANT TO SEARCH GOOGLE PLACES!!!!!!!!
-
-    // Rendering of DateTimePicker on Click of Next to select date and time
-    //  Adjust styling and rendering of this component !!!
-
-    // If there is a From and a To location in state hooks, render Location page with two Text fields
-    //  that contain the To and From coordinates ---> allow both to be clickable and to return to original Location form 
-    //      ---> best way to transition on returning back to form
-    //  Also create a back button (arrow) to be placed to the left of the To and From location Text fields to
-    //      also navigate back to original Location form input fields
-    //  Then allow for Date/Time picker to show, and underneath create a Next and Back button
-    //      only enable Next button as clickable when a date in the future is selected
-    //  If the button "Schedule a Ride Now", do not display the Time/Date picker (set another hook to only render
-    //      selection input fields)
-
-    const GOOGLE_MAPS_APIKEY = 'AIzaSyBpktIvH-LC6Pwrp0ShC7NbjH5AqoySf8s';
+    
+    const GOOGLE_MAPS_APIKEY = '';
 
     // Hooks for storing 'toLocation' and 'fromLocation'
     const [fromLocation, setFrom] = useState('');
@@ -37,9 +24,10 @@ const LocationForm = ({ updateFromLocation, ...props }) => {
     // Hook for mounting initial From location of user
     const [componentDidMount, setMounted] = useState(false);
 
-    const [newFromLocation, changeFrom] = useState(null);
     // Hook to use different From location for rerendering purposes
+    const [newFromLocation, changeFrom] = useState(null);
 
+    // Using user's geoLocation to get their actual address
     getReverseGeocode = async () => {
         if (props.geoLocation) {
             // If component has not mounted, request reverse geolocation
@@ -65,55 +53,33 @@ const LocationForm = ({ updateFromLocation, ...props }) => {
 
     // Get user's reverse geoCoded address
     getReverseGeocode();
-
-    // Setting date for date/time picker (Android)
-    const [currentIoSDate, setIoSDate] = useState(null);
-
-    // Setting date for date/time picker (Android)
-    const [currentAndroidDate, setAndroidDate] = useState(null);
-    const [rideAndroidTime, setAndroidTime] = useState(null);
-
+    
     // Store new From location in hook and in redux
     const updateFromState = (newFromLocation) => {
         setFrom(newFromLocation);
         props.setFrom(fromLocation);
     };
-
+    
     // Store new To location in hook and in redux
     const updateToState = (newToLocation) => {
         setTo(newToLocation);
         props.setTo(toLocation);
     };
+    
+    /* DATE COMPONENTS */
+    
+    // Setting date for date/time picker (Android)
+    const [currentIoSDate, setIoSDate] = useState(null);
+    
+    // Setting date for date/time picker (Android)
+    const [currentAndroidDate, setAndroidDate] = useState(null);
+    const [rideAndroidTime, setAndroidTime] = useState(null);
 
     // Store new Time in hook and in redux
     const updateTimeAndDate = (newToLocation) => {
         setTo(newToLocation);
         props.setTo(toLocation);
     };
-
-    // Alert pop up for dates in the past
-    const dateAlert = (selectedDate) => {
-        let today = new Date();
-        if (selectedDate < today) {
-            Alert.alert(
-                'We\'re Sorry!',
-                'Please select a time in the future to schedule your request.',
-                [
-                    { text: 'OK', onPress: () => console.log('OK Pressed') },
-                ],
-                { cancelable: false },
-            );
-        }
-    }
-
-    // If this is a scheduled request, render the Text Input fields to select a From and To Destination
-    //  then, allow to user to view their destination request on the map
-    //  and then, render Time&Date selector to confirm future request
-    //  and then, ask user to submit request
-    // Else,
-    //  only render the Text Input fields to select a From and To Destination,
-    //  then, allow user to view their desination request on the map,
-    //  and then, ask user to submit request
 
     if (props.scheduled) {
         if (fromLocation !== '' && toLocation !== '') {
@@ -148,7 +114,6 @@ const LocationForm = ({ updateFromLocation, ...props }) => {
                                 currentAndroidDate={currentAndroidDate}
                                 setAndroidDate={setAndroidDate}
                                 setAndroidTime={setAndroidTime}
-                                dateAlert={dateAlert}
                             >
                             </DateAndTimePicker>
                         }
@@ -164,14 +129,44 @@ const LocationForm = ({ updateFromLocation, ...props }) => {
                             </DateAndTimePicker>
                         }
                         <Surface style={styles2.timeAndDateBox}>
-                            {Platform.OS === 'ios' &&
-                                <Text>{currentIoSDate}</Text>
-                            }
-                            {Platform.OS === 'android' &&
-                                <Text>{currentAndroidDate}</Text>
-                            }
-                            <Button style={styles2.backButton}>Hello!</Button>
-                            <Button style={styles2.backButton}>Hello2!</Button>
+                            <View style={styles2.requestBorder}>
+                                {Platform.OS === 'ios' &&
+                                    <TouchableOpacity 
+                                        onPress={() => {
+                                            setIoSDate(null);
+                                        }}>
+                                        <Text style={styles2.requestText}>New Scheduled Request Date:</Text>
+                                        <Text style={styles2.requestText}>
+                                            {Moment(currentIoSDate).format("LLL")}
+                                        </Text>
+                                    </TouchableOpacity>
+                                }
+                                {Platform.OS === 'android' &&
+                                    <TouchableOpacity 
+                                        onPress={() => {
+                                            setAndroidDate(null);
+                                        }}>
+                                        <Text style={styles2.requestText}>New Scheduled Request Date:</Text>
+                                        <Text style={styles2.requestText}>
+                                            {Moment(currentAndroidDate).format("LLL")}
+                                        </Text>
+                                    </TouchableOpacity>
+                                }
+                            </View>
+                            <View style={styles2.buttonBox}>
+                                <Button style={styles2.backButton} onPress={() => {setTo('')}}>Back</Button>
+                                <Button style={styles2.nextButton} 
+                                        onPress={() => {
+                                            if (Platform.OS === 'ios') {
+                                                props.setTime(currentIoSDate)
+                                            } else if (Platform.OS === 'android') {
+                                                props.setTime(currentAndroidDate)
+                                            };
+                                            props.setForm(1);
+                                        }}>
+                                    Next
+                                </Button>
+                            </View>
                         </Surface>
                     </View>
                 </TouchableWithoutFeedback>
@@ -249,7 +244,6 @@ const LocationForm = ({ updateFromLocation, ...props }) => {
                     <Button styles={styles.nextButton}
                         mode='contained'
                         onPress={() => {
-
                             if (!toLocation) {
                                 Alert.alert(
                                     'We\'re Sorry!',
@@ -393,27 +387,66 @@ const styles2 = StyleSheet.create({
         top: '50%',
         width: 500,
         maxHeight: 200,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        textAlign: 'center',
+        alignContent: 'center',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column'
     },
-    backButtonBox: {
+    buttonBox: {
         flex: 1,
         width: 500,
-        height: 50,
+        maxHeight: 200,
         justifyContent: 'center',
         alignItems: 'center',
-        alignSelf: 'center',
-        alignContent: 'center',
+        flexDirection: 'row'
+    },
+    requestText: {
+        fontFamily: Platform.OS === 'ios' ? "Arial" : "Roboto",
+        letterSpacing: 2,
+        fontWeight: "bold",
+        fontSize: 15,
+        lineHeight: 40,
+        color: theme.colors.primary,
+    },
+    requestBorder: {
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        paddingRight: 10,
+        paddingLeft: 10,
+        marginTop: 10
     },
     backButton: {
         flex: 1,
         width: 100,
-        height: 50,
+        height: 70,
+        marginLeft: 50,
+        marginRight: 10,
+        paddingRight: 10,
+        paddingLeft: 10,
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf: 'center',
-    }
+    },
+    nextButton: {
+        flex: 1,
+        width: 100,
+        height: 70,
+        marginLeft: 10,
+        marginRight: 50,
+        borderRadius: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+    },
 });
 
 const mapStateToProps = state => {
